f5676db1a9a50acb42a104f2797b9cb3d1b475a6
Mr.Hope <mister-hope@outlook.com>
2024-01-27 06:56:15 +0800

    chore: add tests

---
+++ tools/helper/tests/node/excerpt.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest'
+import { createBaseApp } from 'vuepress/core'
+import { path } from 'vuepress/utils'
+import { getPageExcerpt } from '../../src/node/page/excerpt.js'
+import { emptyTheme } from '../__fixtures__/theme/empty.js'
+
+describe('Should generate page excerpt correctly', async () => {
+  const app = createBaseApp({
+    bundler: {} as any,
+    source: path.resolve(__dirname, '../__fixtures__/src'),
+    theme: emptyTheme,
+  })
+
+  await app.init()
+
+  it('Should generate excerpt for all pages', () => {
+    app.pages.forEach((page) => {
+      expect(getPageExcerpt(app, page)).toMatchSnapshot()
+    })
+  })
+
+  it('Should only generate excerpt for marker', () => {
+    app.pages.forEach((page) => {
+      expect(getPageExcerpt(app, page, { excerptLength: 0 })).toMatchSnapshot()
+    })
+  })
+})
