d916bf77c2d2765e6f76c36007094b1090379c2a
Mr.Hope <mister-hope@outlook.com>
2024-01-27 06:58:47 +0800

    chore: tweaks

--- tools/helper/src/node/bundler/getBundlerName.ts
+++ tools/helper/src/node/bundler/getBundlerName.ts
@@ -1,7 +1,7 @@
 import type { App } from 'vuepress/core'
 
-export const getBundlerName = ({
-  options: {
-    bundler: { name },
-  },
-}: App): string => name.match(/^@vuepress\/bundler-(.*)$/)?.[1] || name
+export const getBundlerName = (app: App): string => {
+  const { name } = app.options.bundler
+
+  return name.match(/^@vuepress\/bundler-(.*)$/)?.[1] || name
+}
