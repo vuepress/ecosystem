fadf1225c643ab0bf9c4c0f46d0b3b3f1aa15ac6
Mr.Hope <mister-hope@outlook.com>
2024-01-29 12:55:32 +0800

    feat(helper): add helper package (#33)

---
+++ tools/helper/src/node/index.ts
@@ -0,0 +1,5 @@
+export const noopComponent = '@vuepress/helper/noopComponent'
+export const noopModule = '@vuepress/helper/noopModule'
+
+export * from './bundler/index.js'
+export * from '../shared/index.js'
---
+++ tools/helper/src/node/utils/index.ts
@@ -0,0 +1,1 @@
+export * from './packageManager.js'
