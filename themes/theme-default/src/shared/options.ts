c498cdbe93600c34ca0b4b2f4b5c123854077896
Mr.Hope <mister-hope@outlook.com>
2024-02-01 03:53:48 +0800

    chore: update

--- themes/theme-default/src/shared/options.ts
+++ themes/theme-default/src/shared/options.ts
@@ -28,6 +28,9 @@ export interface DefaultThemePluginsOptions {
     codeGroupItem?: boolean
   }
 
+  /**
+   * Enable @vuepress/plugin-copy-code or not
+   */
   copyCode?: CopyCodePluginOptions | boolean
 
   /**
