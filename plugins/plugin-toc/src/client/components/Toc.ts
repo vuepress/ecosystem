eb99446e1626fd30134e2d6b8e7ca79ef7a1eb90
Mr.Hope <mister-hope@outlook.com>
2024-02-03 20:44:36 +0800

    test: update snapshots

--- plugins/plugin-toc/src/client/components/Toc.ts
+++ plugins/plugin-toc/src/client/components/Toc.ts
@@ -3,7 +3,7 @@ import type { PropType, VNode } from 'vue'
 import { RouterLink, useRoute } from 'vue-router'
 import type { RouteLocationNormalizedLoaded } from 'vue-router'
 import type { PageHeader } from 'vuepress/client'
-import { usePageData } from 'vuepress/client'
+import { usePageData, VPLink } from 'vuepress/client'
 import type { TocPropsOptions } from '../../shared/index.js'
 
 export type TocPropsHeaders = PageHeader[]
@@ -32,6 +32,18 @@ const renderLink = (
     header.children.some((item) => `#${item.slug}` === route.hash)
   ) {
     linkClass.push(options.linkChildrenActiveClass)
+  }
+
+  if (options.linkTag === 'VPLink') {
+    return h(
+      VPLink,
+      {
+        to: hash,
+        class: linkClass,
+        ariaLabel: header.title,
+      },
+      () => header.title,
+    )
   }
 
   if (options.linkTag === 'RouterLink') {
@@ -123,7 +135,7 @@ export const Toc = defineComponent({
       containerClass: 'vuepress-toc',
       listClass: 'vuepress-toc-list',
       itemClass: 'vuepress-toc-item',
-      linkTag: 'RouterLink',
+      linkTag: 'VPLink',
       linkClass: 'vuepress-toc-link',
       linkActiveClass: 'active',
       linkChildrenActiveClass: 'active',
