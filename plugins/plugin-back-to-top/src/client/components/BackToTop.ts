070aea2ee30d63509732bc38e5287578225ef009
Mr.Hope <mister-hope@outlook.com>
2024-02-03 15:56:43 +0800

    feat(plugin-back-to-top): rebuild plugin

--- plugins/plugin-back-to-top/src/client/components/BackToTop.ts
+++ plugins/plugin-back-to-top/src/client/components/BackToTop.ts
@@ -1,37 +1,93 @@
-import { debounce } from 'ts-debounce'
-import { computed, defineComponent, h, onMounted, ref, Transition } from 'vue'
-import { getScrollTop, scrollToTop } from '../utils.js'
+import { useLocaleConfig } from '@vuepress/helper/client'
+import { useElementSize, useWindowScroll, useWindowSize } from '@vueuse/core'
+import {
+  computed,
+  defineComponent,
+  h,
+  onMounted,
+  shallowRef,
+  Transition,
+} from 'vue'
+import { usePageFrontmatter } from 'vuepress/client'
+import type { BackToTopPluginLocaleConfig } from '../../shared/index.js'
 
-import '../styles/vars.css'
 import '../styles/back-to-top.css'
+
+declare const __BACK_TO_TOP_LOCALES__: BackToTopPluginLocaleConfig
+declare const __BACK_TO_TOP_PROGRESS__: boolean
+declare const __BACK_TO_TOP_THRESHOLD__: number
 
 export const BackToTop = defineComponent({
   name: 'BackToTop',
 
-  setup() {
-    const scrollTop = ref(0)
-    const show = computed(() => scrollTop.value > 300)
-    const onScroll = debounce(() => {
-      scrollTop.value = getScrollTop()
-    }, 100)
+  setup(props) {
+    const pageFrontmatter = usePageFrontmatter<{ backToTop?: boolean }>()
+    const locale = useLocaleConfig(__BACK_TO_TOP_LOCALES__)
+    const body = shallowRef<HTMLElement>()
+    const { height: bodyHeight } = useElementSize(body)
+    const { height: windowHeight } = useWindowSize()
+
+    /** Scroll distance */
+    const { y } = useWindowScroll()
+
+    /** Whether to display button */
+    const show = computed(
+      () =>
+        pageFrontmatter.value.backToTop !== false &&
+        y.value > __BACK_TO_TOP_THRESHOLD__,
+    )
+
+    const progress = computed(
+      () => (y.value / (bodyHeight.value - windowHeight.value)) * 100,
+    )
 
     onMounted(() => {
-      scrollTop.value = getScrollTop()
-
-      window.addEventListener('scroll', () => onScroll())
+      body.value = document.body
     })
 
-    const backToTopEl = h('div', { class: 'back-to-top', onClick: scrollToTop })
-
     return () =>
-      h(
-        Transition,
-        {
-          name: 'back-to-top',
-        },
-        () => (show.value ? backToTopEl : null),
+      h(Transition, { name: 'back-to-top' }, () =>
+        show.value
+          ? h(
+              'button',
+              {
+                'type': 'button',
+                'class': 'vp-back-to-top-button',
+                'aria-label': locale.value.backToTop,
+                'onClick': () => {
+                  window.scrollTo({ top: 0, behavior: 'smooth' })
+                },
+              },
+              [
+                __BACK_TO_TOP_PROGRESS__
+                  ? h(
+                      'span',
+                      {
+                        'class': 'vp-scroll-progress',
+                        'role': 'progressbar',
+                        'aria-labelledby': 'loadinglabel',
+                        'aria-valuenow': progress.value,
+                      },
+                      h(
+                        'svg',
+                        h('circle', {
+                          cx: '50%',
+                          cy: '50%',
+                          style: {
+                            'stroke-dasharray': `calc(${
+                              Math.PI * progress.value
+                            }% - ${4 * Math.PI}px) calc(${Math.PI * 100}% - ${
+                              4 * Math.PI
+                            }px)`,
+                          },
+                        }),
+                      ),
+                    )
+                  : null,
+                h('div', { class: 'back-to-top-icon' }),
+              ],
+            )
+          : null,
       )
   },
 })
-
-export default BackToTop
--- plugins/plugin-back-to-top/src/node/backToTopPlugin.ts
+++ plugins/plugin-back-to-top/src/node/backToTopPlugin.ts
@@ -1,10 +1,31 @@
+import { getLocaleConfig } from '@vuepress/helper'
 import type { Plugin } from 'vuepress/core'
-import { getDirname, path } from 'vuepress/utils'
+import { getDirname, logger, path } from 'vuepress/utils'
+import { backToTopLocales } from './locales.js'
+import { PLUGIN_NAME } from './logger.js'
+import type { BackToTopPluginOptions } from './options.js'
 
 const __dirname = getDirname(import.meta.url)
 
-export const backToTopPlugin = (): Plugin => ({
-  name: '@vuepress/plugin-back-to-top',
+export const backToTopPlugin =
+  (options: BackToTopPluginOptions = {}): Plugin =>
+  (app) => {
+    if (app.env.isDebug) logger.info('Options', options)
 
-  clientConfigFile: path.resolve(__dirname, '../client/config.js'),
-})
+    return {
+      name: PLUGIN_NAME,
+
+      define: (app) => ({
+        __BACK_TO_TOP_LOCALES__: getLocaleConfig({
+          app,
+          name: 'back-to-top',
+          default: backToTopLocales,
+          config: options.locales,
+        }),
+        __BACK_TO_TOP_PROGRESS__: options.progress ?? true,
+        __BACK_TO_TOP_THRESHOLD__: options.threshold ?? 100,
+      }),
+
+      clientConfigFile: path.resolve(__dirname, '../client/config.js'),
+    }
+  }
