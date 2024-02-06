433f2d238e5b20e4717f548a597bc19567490c35
Mr.Hope <mister-hope@outlook.com>
2024-02-06 19:06:14 +0800

    feat(plugin-photo-swipe): add photo-swipe plugin

---
+++ plugins/plugin-photo-swipe/src/client/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './createPhotoSwipe.js'
+export * from './images.js'
+export * from './usePhotoSwipe.js'
---
+++ plugins/plugin-photo-swipe/src/client/utils/usePhotoSwipe.ts
@@ -0,0 +1,71 @@
+import { useEventListener } from '@vueuse/core'
+import type PhotoSwipe from 'photoswipe'
+import type { SlideData } from 'photoswipe'
+import type { PhotoSwipeOptions } from '../helpers/index.js'
+import { LOADING_ICON } from './icon.js'
+import { getImageElementInfo } from './images.js'
+import { initPhotoSwipe } from './initPhotoSwipe.js'
+
+export const registerPhotoSwipe = (
+  images: HTMLImageElement[],
+  photoSwipeOptions: PhotoSwipeOptions,
+  scrollToClose = true,
+): Promise<() => void> =>
+  import(/* webpackChunkName: "photo-swipe" */ 'photoswipe').then(
+    ({ default: PhotoSwipe }) => {
+      let currentPhotoSwipe: PhotoSwipe | null = null
+
+      const dataSource = images.map<SlideData>((image) => ({
+        html: LOADING_ICON,
+        element: image,
+        msrc: image.src,
+      }))
+
+      images.forEach((image, index) => {
+        const handler = (): void => {
+          currentPhotoSwipe?.destroy()
+          currentPhotoSwipe = new PhotoSwipe({
+            preloaderDelay: 0,
+            showHideAnimationType: 'zoom',
+            ...photoSwipeOptions,
+            dataSource,
+            index,
+            ...(scrollToClose
+              ? { closeOnVerticalDrag: true, wheelToZoom: false }
+              : {}),
+          })
+
+          initPhotoSwipe(currentPhotoSwipe)
+
+          currentPhotoSwipe.addFilter('thumbEl', () => image)
+          currentPhotoSwipe.addFilter('placeholderSrc', () => image.src)
+          currentPhotoSwipe.init()
+        }
+
+        if (!image.getAttribute('photo-swipe')) {
+          image.style.cursor = 'zoom-in'
+          image.addEventListener('click', () => {
+            handler()
+          })
+          image.addEventListener('keypress', ({ key }) => {
+            if (key === 'Enter') handler()
+          })
+          // avoid registering multiple times
+          image.setAttribute('photo-swipe', '')
+        }
+
+        getImageElementInfo(image).then((data) => {
+          dataSource.splice(index, 1, data)
+          currentPhotoSwipe?.refreshSlideContent(index)
+        })
+      })
+
+      return scrollToClose
+        ? useEventListener('wheel', () => {
+            currentPhotoSwipe?.close()
+          })
+        : (): void => {
+            // do nothing
+          }
+    },
+  )
