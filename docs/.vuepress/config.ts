7def0d3bc46f634102a72a3bfba943fdbb101e7a
Mr.Hope <mister-hope@outlook.com>
2024-03-10 23:30:25 +0800

    docs: add docsearch

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,8 @@ import { footnote } from '@mdit/plugin-footnote'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 import { catalogPlugin } from '@vuepress/plugin-catalog'
-// import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { feedPlugin } from '@vuepress/plugin-feed'
 import { redirectPlugin } from '@vuepress/plugin-redirect'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { searchPlugin } from '@vuepress/plugin-search'
@@ -75,57 +76,59 @@ export default defineUserConfig({
   // use plugins
   plugins: [
     catalogPlugin(),
-    // docsearchPlugin({
-    //   appId: '34YFD9IUQ2',
-    //   apiKey: '9a9058b8655746634e01071411c366b8',
-    //   indexName: 'vuepress',
-    //   searchParameters: {
-    //     facetFilters: ['tags:v2'],
-    //   },
-    //   locales: {
-    //     '/zh/': {
-    //       placeholder: '搜索文档',
-    //       translations: {
-    //         button: {
-    //           buttonText: '搜索文档',
-    //           buttonAriaLabel: '搜索文档',
-    //         },
-    //         modal: {
-    //           searchBox: {
-    //             resetButtonTitle: '清除查询条件',
-    //             resetButtonAriaLabel: '清除查询条件',
-    //             cancelButtonText: '取消',
-    //             cancelButtonAriaLabel: '取消',
-    //           },
-    //           startScreen: {
-    //             recentSearchesTitle: '搜索历史',
-    //             noRecentSearchesText: '没有搜索历史',
-    //             saveRecentSearchButtonTitle: '保存至搜索历史',
-    //             removeRecentSearchButtonTitle: '从搜索历史中移除',
-    //             favoriteSearchesTitle: '收藏',
-    //             removeFavoriteSearchButtonTitle: '从收藏中移除',
-    //           },
-    //           errorScreen: {
-    //             titleText: '无法获取结果',
-    //             helpText: '你可能需要检查你的网络连接',
-    //           },
-    //           footer: {
-    //             selectText: '选择',
-    //             navigateText: '切换',
-    //             closeText: '关闭',
-    //             searchByText: '搜索提供者',
-    //           },
-    //           noResultsScreen: {
-    //             noResultsText: '无法找到相关结果',
-    //             suggestedQueryText: '你可以尝试查询',
-    //             reportMissingResultsText: '你认为该查询应该有结果？',
-    //             reportMissingResultsLinkText: '点击反馈',
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
-    // }),
+    docsearchPlugin({
+      appId: 'N7UOPMVZ5B',
+      apiKey: 'aa626dfa43a5e32cd519ba84735ad384',
+      indexName: 'ecosystem-vuejs',
+      locales: {
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
+        },
+      },
+    }),
+    feedPlugin({
+      atom: true,
+      json: true,
+      rss: true,
+    }),
     redirectPlugin({
       switchLocale: 'modal',
     }),
