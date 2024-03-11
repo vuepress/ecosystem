5adfb7db983a702462e7126c8b541be210ac969b
Mr.Hope <mister-hope@outlook.com>
2024-03-11 15:37:15 +0800

    chore: tweaks

--- plugins/plugin-comment/src/client/pageview.ts
+++ plugins/plugin-comment/src/client/pageview.ts
@@ -1,15 +1,14 @@
 import type { WalineAbort } from '@waline/client'
-import type { WalineOptions } from '../shared/index.js'
-
-declare const __COMMENT_OPTIONS__: WalineOptions
+import { useWalineOptions } from './helpers/index.js'
 
 export const updatePageview = async (): Promise<WalineAbort | void> => {
   try {
     const { pageviewCount } = await import(
       /* webpackChunkName: "pageview" */ '@waline/client/pageview'
     )
+    const commentOptions = useWalineOptions()
 
-    return pageviewCount({ serverURL: __COMMENT_OPTIONS__.serverURL })
+    return pageviewCount({ serverURL: commentOptions.serverURL })
   } catch (err) {
     console.error('@waline/client is not installed!')
   }
