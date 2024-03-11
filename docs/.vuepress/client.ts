7a277b778495ce076c83177a8575765b52605da0
Mr.Hope <mister-hope@outlook.com>
2024-03-11 15:27:37 +0800

    chore: update discussion

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -4,10 +4,10 @@ import type { ClientConfig } from 'vuepress/client'
 import CommentPage from './layouts/CommentPage.vue'
 
 defineGiscusConfig({
-  repo: 'vuepress-theme-hope/giscus-discussions',
-  repoId: 'R_kgDOG_Pt2A',
+  repo: 'vuepress/ecosystem',
+  repoId: 'R_kgDOKPxScA',
   category: 'Announcements',
-  categoryId: 'DIC_kwDOG_Pt2M4COD69',
+  categoryId: 'DIC_kwDOKPxScM4CbWy7',
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
