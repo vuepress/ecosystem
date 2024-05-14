69ee64ba22a0603e677b0b69e0b759c1dfe0e8f0
Mister-Hope <mister-hope@outlook.com>
2024-05-14 15:39:07 +0800

    feat: reduce style

--- plugins/plugin-markdown-image/src/node/prepare/prepareFigureStyleFile.ts
+++ plugins/plugin-markdown-image/src/node/prepare/prepareFigureStyleFile.ts
@@ -8,36 +8,17 @@ export const prepareFigureStyleFile = async (
     'markdown-image/figure.css',
     `\
 ${contentSelector} figure {
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  width: auto;
-  margin: 1rem auto;
-
   text-align: center;
 }
 
+figcaption {
+  font-size: 0.8rem;
+}
+
+/* hide external-link-icon */
 ${contentSelector} figure > a[href*="://"]::after,
 ${contentSelector} figure > a[target=_blank]::after {
   display: none !important;
-}
-
-${contentSelector} figure img {
-  overflow: hidden;
-  margin: 0 auto;
-  border-radius: 8px;
-}
-
-${contentSelector} figure img[tabindex]:focus {
-  outline: 2px solid var(--theme-color, #3eaf7c);
-}
-
-${contentSelector} figure figcaption {
-  display: inline-block;
-  margin: 6px auto;
-  font-size: 0.8rem;
 }
 `,
   )
