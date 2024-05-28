f158302e7a3dcde00109651bcbefa90c1211a940
Mister-Hope <mister-hope@outlook.com>
2024-05-28 16:12:55 +0800

    chore: fix commit lint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -6,7 +6,11 @@ const getSubDirectories = (dir) =>
     .readdirSync(dir)
     .filter((item) => fs.statSync(path.join(dir, item)).isDirectory())
 
-const pluginPackages = getSubDirectories(path.resolve(__dirname, 'plugins'))
+const pluginPackages = getSubDirectories(
+  path.resolve(__dirname, 'plugins'),
+).flatMap((category) =>
+  getSubDirectories(path.resolve(__dirname, 'plugins', category)),
+)
 const themePackages = getSubDirectories(path.resolve(__dirname, 'themes'))
 const toolsPackages = getSubDirectories(path.resolve(__dirname, 'tools'))
 
