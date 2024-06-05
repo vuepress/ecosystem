e5dcba2c1ad03ef3083fcfc3bcc8d0105793f270
Mister-Hope <mister-hope@outlook.com>
2024-06-05 14:03:36 +0800

    fix(theme-default): fix client config file import

--- themes/theme-default/src/client/config.ts
+++ themes/theme-default/src/client/config.ts
@@ -1,13 +1,11 @@
+import { setupDarkMode } from '@theme/useDarkMode'
+import { useScrollPromise } from '@theme/useScrollPromise'
+import { setupSidebarItems } from '@theme/useSidebarItems'
 import { hasGlobalComponent } from '@vuepress/helper/client'
 import { h } from 'vue'
 import { defineClientConfig } from 'vuepress/client'
 import type { ClientConfig } from 'vuepress/client'
 import { Badge, CodeGroup, CodeGroupItem } from './components/global/index.js'
-import {
-  setupDarkMode,
-  setupSidebarItems,
-  useScrollPromise,
-} from './composables/index.js'
 import Layout from './layouts/Layout.vue'
 import NotFound from './layouts/NotFound.vue'
 
