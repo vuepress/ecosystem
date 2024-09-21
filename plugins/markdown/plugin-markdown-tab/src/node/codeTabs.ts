49ea97cf10f07ad3e20b2528d17efc3b6576ac7a
Mister-Hope <mister-hope@outlook.com>
2024-09-21 23:23:02 +0800

    feat: add plugin-markdown-tab (#250)

---
+++ plugins/markdown/plugin-markdown-tab/src/node/codeTabs.ts
@@ -0,0 +1,69 @@
+import { tab } from '@mdit/plugin-tab'
+import type { PluginSimple } from 'markdown-it'
+
+import { stringifyProp } from './utils.js'
+
+export const codeTabs: PluginSimple = (md) => {
+  tab(md, {
+    name: 'code-tabs',
+
+    tabsOpenRenderer: ({ active, data }, tokens, index) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { meta } = tokens[index]
+      const titles = data.map(({ title }) => md.renderInline(title))
+      const tabsData = data.map((item, dataIndex) => {
+        const { id = titles[dataIndex] } = item
+
+        return { id }
+      })
+
+      return `<CodeTabs id="${index}" :data='${stringifyProp(tabsData)}'${
+        active === -1 ? '' : ` :active="${active}"`
+      }${
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        meta.id ? ` tab-id="${meta.id as string}"` : ''
+      }>
+${titles
+  .map(
+    (title, titleIndex) => `\
+<template #title${titleIndex}="{ value, isActive }">${title}</template>
+`,
+  )
+  .join('')}\
+`
+    },
+
+    tabsCloseRenderer: () => `\
+</CodeTabs>
+`,
+
+    tabOpenRenderer: ({ index }, tokens, tokenIndex) => {
+      let foundFence = false
+
+      // Hide all elements excerpt the first fence
+      for (let i = tokenIndex; i < tokens.length; i++) {
+        const { block, type } = tokens[i]
+
+        if (block) {
+          if (type === 'code-tabs_tab_close') break
+
+          if ((type === 'fence' || type === 'import_code') && !foundFence) {
+            foundFence = true
+            continue
+          }
+
+          tokens[i].type = 'code_tab_empty'
+          tokens[i].hidden = true
+        }
+      }
+
+      return `\
+<template #tab${index}="{ value, isActive }">
+`
+    },
+
+    tabCloseRenderer: () => `\
+</template>
+`,
+  })
+}
