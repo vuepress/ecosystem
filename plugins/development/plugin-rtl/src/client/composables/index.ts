3d0e4a18263a9daa0a2b49b1d40fccc219357997
Mr.Hope <mister-hope@outlook.com>
2024-09-29 16:48:23 +0800

    feat(plugin-rtl): provide composables

---
+++ plugins/development/plugin-rtl/src/client/composables/index.ts
@@ -0,0 +1,1 @@
+export * from './useRtl.js'
---
+++ plugins/development/plugin-rtl/src/client/composables/useRtl.ts
@@ -0,0 +1,51 @@
+import { entries } from '@vuepress/helper'
+import { onMounted, watch } from 'vue'
+import { useRouteLocale } from 'vuepress/client'
+import { getElement } from '../utils/index.js'
+
+/**
+ *
+ * @param rtlLocalePaths rtl locale paths
+ * @param selectorOptions rtl selector options
+ */
+export const useRtl = (
+  rtlLocalePaths: string[],
+  selectorOptions?: Record<string, Record<string, string>>,
+): void => {
+  const routeLocale = useRouteLocale()
+
+  const toggleRTL = (localePath: string): void => {
+    if (rtlLocalePaths.includes(localePath)) {
+      entries(
+        selectorOptions ?? {
+          html: { dir: 'rtl' },
+        },
+      ).forEach(([selector, attrs = {}]) => {
+        const element = getElement(selector)
+
+        if (element)
+          entries(attrs).forEach(([attr, value]) => {
+            if (attr === 'class') element.classList.add(value)
+            else element.setAttribute(attr, value)
+          })
+      })
+      document.documentElement.style.setProperty('direction', 'rtl')
+    } else {
+      entries(rtlLocalePaths).forEach(([selector, attrs = {}]) => {
+        const element = getElement(selector)
+
+        if (element)
+          entries(attrs).forEach(([attr, value]) => {
+            if (attr === 'class') element.classList.remove(value)
+            else element.removeAttribute(attr)
+          })
+      })
+
+      document.documentElement.style.removeProperty('direction')
+    }
+  }
+
+  onMounted(() => {
+    watch(routeLocale, toggleRTL, { immediate: true })
+  })
+}
