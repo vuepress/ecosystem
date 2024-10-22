1ebe65d10dcdd697ed00e6b90df9cd919f40fb5c
Mister-Hope <mister-hope@outlook.com>
2024-10-22 19:42:30 +0800

    feat: add plugin-markdown-ext

---
+++ plugins/markdown/plugin-markdown-ext/tests/node/vPre.spec.ts
@@ -0,0 +1,14 @@
+import MarkdownIt from 'markdown-it'
+import { describe, expect, it } from 'vitest'
+
+import { vPre } from '../../src/node/markdown-it-plugins/vPre.js'
+
+describe('v-pre', () => {
+  it('should add v-pre wrapper', () => {
+    const markdownIt = MarkdownIt({ linkify: true }).use(vPre)
+
+    expect(markdownIt.render('::: v-pre\n{{a}}\n:::\n')).toBe(
+      '<div v-pre>\n<p>{{a}}</p>\n</div>\n',
+    )
+  })
+})
