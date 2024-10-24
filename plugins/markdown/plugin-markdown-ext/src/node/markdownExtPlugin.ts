4171e82a806049ef0667794311a395889aebd716
Mister-Hope <mister-hope@outlook.com>
2024-10-24 22:58:18 +0800

    feat(plugin-markdown-ext): add styles for footnote and tasklist

--- plugins/markdown/plugin-markdown-ext/src/node/markdownExtPlugin.ts
+++ plugins/markdown/plugin-markdown-ext/src/node/markdownExtPlugin.ts
@@ -8,6 +8,7 @@ import {
   vPre as vPrePlugin,
 } from './markdown-it-plugins/index.js'
 import type { MarkdownExtPluginOptions } from './options.js'
+import { prepareClientConfigFile } from './prepreClientConfigFile.js'
 import { PLUGIN_NAME } from './utils.js'
 
 export const markdownExtPlugin = ({
@@ -16,23 +17,27 @@ export const markdownExtPlugin = ({
   linkify,
   footnote,
   tasklist,
-
   component,
   vPre,
-}: MarkdownExtPluginOptions): Plugin => {
-  return {
-    name: PLUGIN_NAME,
+}: MarkdownExtPluginOptions): Plugin => ({
+  name: PLUGIN_NAME,
 
-    extendsMarkdown: (md) => {
-      // Behavior
-      if (breaks ?? gfm) md.options.breaks = true
-      if (linkify ?? gfm) md.options.linkify = true
+  extendsMarkdown: (md) => {
+    // Behavior
+    if (breaks ?? gfm) md.options.breaks = true
+    if (linkify ?? gfm) md.options.linkify = true
 
-      if (footnote ?? gfm) md.use(footnotePlugin)
-      if (tasklist ?? gfm)
-        md.use(tasklistPlugin, [isPlainObject(tasklist) ? tasklist : {}])
-      if (component) md.use(componentPlugin)
-      if (vPre) md.use(vPrePlugin)
-    },
-  }
-}
+    if (footnote ?? gfm) md.use(footnotePlugin)
+    if (tasklist ?? gfm)
+      md.use(tasklistPlugin, [isPlainObject(tasklist) ? tasklist : {}])
+    if (component) md.use(componentPlugin)
+    if (vPre) md.use(vPrePlugin)
+  },
+
+  clientConfigFile: (app) =>
+    prepareClientConfigFile(app, {
+      gfm,
+      footnote,
+      tasklist,
+    }),
+})
