3f7034ef18f0fa6a50969846465bcf88d8849a7a
pengzhanbo <volodymyr@foxmail.com>
2024-11-03 13:07:15 +0800

    chore: tweak

--- plugins/development/plugin-git/src/node/utils/checkGitRepo.ts
+++ plugins/development/plugin-git/src/node/utils/checkGitRepo.ts
@@ -13,21 +13,43 @@ export const checkGitRepo = (cwd: string): boolean => {
   }
 }
 
+const getRemoteUrl = (cwd: string): string => {
+  try {
+    const { stdout } = execaCommandSync('git remote get-url origin', { cwd })
+    return stdout
+  } catch {
+    try {
+      const { stdout } = execaCommandSync('git remote', { cwd })
+      const remote = stdout.split('\n')[0]?.trim()
+      if (remote) {
+        const { stdout: remoteUrl } = execaCommandSync(
+          `git remote get-url ${remote}`,
+          {
+            cwd,
+          },
+        )
+        return remoteUrl
+      }
+      return ''
+    } catch {
+      return ''
+    }
+  }
+}
+
 export const referGitProvider = (cwd: string): KnownGitProvider | null => {
   try {
-    const { stdout } = execaCommandSync('git remote get-url origin', {
-      cwd,
-    })
-    if (stdout.includes('github.com')) {
+    const remoteUrl = getRemoteUrl(cwd)
+    if (remoteUrl.includes('github.com')) {
       return 'github'
     }
-    if (stdout.includes('gitlab.com')) {
+    if (remoteUrl.includes('gitlab.com')) {
       return 'gitlab'
     }
-    if (stdout.includes('gitee.com')) {
+    if (remoteUrl.includes('gitee.com')) {
       return 'gitee'
     }
-    if (stdout.includes('bitbucket.org')) {
+    if (remoteUrl.includes('bitbucket.org')) {
       return 'bitbucket'
     }
     return null
