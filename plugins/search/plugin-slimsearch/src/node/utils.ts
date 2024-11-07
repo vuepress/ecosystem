cae6598e67b438dcde5cd7046cc4daabce12b547
Mister-Hope <mister-hope@outlook.com>
2024-11-08 03:05:44 +0800

    chore: tweaks

--- plugins/search/plugin-slimsearch/src/node/utils.ts
+++ plugins/search/plugin-slimsearch/src/node/utils.ts
@@ -3,15 +3,15 @@ import { getDirname, path } from 'vuepress/utils'
 
 const __dirname = getDirname(import.meta.url)
 
+export const PLUGIN_NAME = '@vuepress/plugin-slimsearch'
+
+export const logger = new Logger(PLUGIN_NAME)
+
 export const CLIENT_FOLDER = ensureEndingSlash(
   path.resolve(__dirname, '../client/'),
 )
 
 export const WORKER_FILE = path.resolve(__dirname, '../worker/index.js')
-
-export const PLUGIN_NAME = '@vuepress/plugin-slimsearch'
-
-export const logger = new Logger(PLUGIN_NAME)
 
 export const getLocaleChunkName = (locale: string): string =>
   locale.replace(/\//g, '') || 'root'
