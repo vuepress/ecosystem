4ce00fe7a926913f7d886a0faf75d233377acfc0
Mister-Hope <mister-hope@outlook.com>
2024-11-29 01:13:58 +0800

    chore: tweaks

--- themes/theme-next/src/shared/navbar.ts
+++ themes/theme-next/src/shared/navbar.ts
@@ -4,17 +4,6 @@ export interface NavItemWithLink {
   text: string
   link: string
 
-  prefix?: never
-
-  items?: never
-
-  /**
-   * @deprecated Use `items` instead
-   *
-   * @deprecated 使用 `items` 替换
-   *  */
-  children?: never
-
   /**
    * A Regexp string, matching path will be active
    *
@@ -29,6 +18,15 @@ export interface NavItemWithLink {
   rel?: string
   target?: string
   noIcon?: boolean
+
+  prefix?: never
+  items?: never
+  /**
+   * @deprecated Use `items` instead
+   *
+   * @deprecated 使用 `items` 替换
+   *  */
+  children?: never
 }
 
 export interface NavItemChildren {
@@ -69,6 +67,17 @@ export interface NavItemWithChildren {
   prefix?: string
 
   /**
+   * A Regexp string, matching path will be active
+   *
+   * It's expected to be a regex string as RegExp object isn't serializable
+   *
+   * 正则表达式字符串，匹配的路径将被激活
+   *
+   * 由于 RegExp 对象不可序列化，因此我们需要将其定义为字符串。
+   */
+  activeMatch?: string
+
+  /**
    * The items in the dropdown menu
    *
    * 导航栏下拉菜单
@@ -81,15 +90,4 @@ export interface NavItemWithChildren {
    * @deprecated 使用 `items` 替换
    *  */
   children?: (NavItemChildren | NavItemWithLink | string)[]
-
-  /**
-   * A Regexp string, matching path will be active
-   *
-   * It's expected to be a regex string as RegExp object isn't serializable
-   *
-   * 正则表达式字符串，匹配的路径将被激活
-   *
-   * 由于 RegExp 对象不可序列化，因此我们需要将其定义为字符串。
-   */
-  activeMatch?: string
 }
