8202e4aab4773986aa771db0ba930c7db6cc85df
Mister-Hope <mister-hope@outlook.com>
2024-12-06 18:44:41 +0800

    chore: tweaks

--- tools/helper/src/node/utils/getModulePath.ts
+++ tools/helper/src/node/utils/getModulePath.ts
@@ -1,7 +1,8 @@
+import { fileURLToPath } from 'node:url'
 import { path } from 'vuepress/utils'
 
 /**
  * Get file path of a given module url
  */
 export const getModulePath = (module: string, meta: ImportMeta): string =>
-  path.normalize(meta.resolve(module))
+  path.normalize(fileURLToPath(meta.resolve(module)))
