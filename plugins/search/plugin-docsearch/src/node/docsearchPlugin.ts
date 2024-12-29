cc734fba5adcd3f003db84d71723066695c501cf
Mister-Hope <mister-hope@outlook.com>
2024-12-29 15:58:44 +0800

    feat(plugin-docsearch): add built-in locale (#323)

--- plugins/search/plugin-docsearch/src/node/docsearchPlugin.ts
+++ plugins/search/plugin-docsearch/src/node/docsearchPlugin.ts
@@ -1,5 +1,7 @@
+import { getFullLocaleConfig } from '@vuepress/helper'
 import type { Plugin } from 'vuepress/core'
 import type { DocSearchOptions } from '../shared/index.js'
+import { docSearchLocaleInfo } from './locales.js'
 import { prepareClientConfig } from './prepareClientConfig.js'
 import { PLUGIN_NAME } from './utils.js'
 
@@ -21,13 +23,22 @@ export interface DocSearchPluginOptions extends DocSearchOptions {
 export const docsearchPlugin = ({
   injectStyles = true,
   indexBase,
+  locales = {},
   ...options
 }: DocSearchPluginOptions = {}): Plugin => ({
   name: PLUGIN_NAME,
 
   define: (app) => ({
     __DOCSEARCH_INDEX_BASE__: indexBase || app.siteData.base,
-    __DOCSEARCH_OPTIONS__: options,
+    __DOCSEARCH_OPTIONS__: {
+      ...options,
+      locales: getFullLocaleConfig({
+        app,
+        default: docSearchLocaleInfo,
+        name: PLUGIN_NAME,
+        config: locales,
+      }),
+    },
   }),
 
   clientConfigFile: (app) => prepareClientConfig(app, injectStyles),
