e8cc7852792857f3a9a49e4efa2708cff374c38b
Mister-Hope <mister-hope@outlook.com>
2025-01-15 14:19:10 +0800

    build(plugin-icon): fix rollup config

--- plugins/features/plugin-icon/rollup.config.ts
+++ plugins/features/plugin-icon/rollup.config.ts
@@ -1,3 +1,6 @@
 import { rollupBundle } from '../../../scripts/rollup.js'
 
-export default [...rollupBundle('node/index'), ...rollupBundle('client/index')]
+export default [
+  ...rollupBundle('node/index', { external: ['@mdit/plugin-icon'] }),
+  ...rollupBundle('client/index'),
+]
