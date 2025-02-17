13925c33c0c384ea9eb63231de5feba27fae1763
Mister-Hope <mister-hope@outlook.com>
2025-02-17 12:09:08 +0800

    docs: update docs

--- docs/.vuepress/layouts/CommentPage.vue
+++ docs/.vuepress/layouts/CommentPage.vue
@@ -1,28 +1,8 @@
 <script setup lang="ts">
+import { useDarkMode } from '@vuepress/theme-default/client'
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
-import { onMounted, onUnmounted, ref } from 'vue'
 
-const isDarkMode = ref(false)
-
-onMounted(() => {
-  const html = document.documentElement
-
-  isDarkMode.value = html.classList.contains('dark')
-
-  // Watch theme change
-  const observer = new MutationObserver(() => {
-    isDarkMode.value = html.classList.contains('dark')
-  })
-
-  observer.observe(html, {
-    attributeFilter: ['class'],
-    attributes: true,
-  })
-
-  onUnmounted(() => {
-    observer.disconnect()
-  })
-})
+const isDarkMode = useDarkMode()
 </script>
 
 <template>
