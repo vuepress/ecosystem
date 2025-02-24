5fa4203b82614d1e27d8e1818c3093d1db1567c5
Mister-Hope <mister-hope@outlook.com>
2025-02-25 02:47:43 +0800

    fix(create-vuepress): fix package.json reading

--- tools/create-vuepress/src/utils/pkgJson.ts
+++ tools/create-vuepress/src/utils/pkgJson.ts
@@ -1,5 +1,16 @@
-import pkg from '../../package.json' with { type: 'json' }
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'node:url'
 
-const { peerDependencies, version } = pkg
+interface PackageJson extends Record<string, unknown> {
+  version: string
+  devDependencies: Record<string, string>
+}
 
-export { peerDependencies, version }
+const pkg = JSON.parse(
+  readFileSync(
+    fileURLToPath(import.meta.resolve('create-vuepress/package.json')),
+    'utf-8',
+  ),
+) as PackageJson
+
+export const { peerDependencies, version } = pkg
