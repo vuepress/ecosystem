377d77035120c860fb571fc4140fb87f8094d756
Mister-Hope <mister-hope@outlook.com>
2025-02-24 12:50:42 +0800

    feat(plugin-medium-zoom)!: remove delay options (#363)

--- plugins/features/plugin-medium-zoom/src/node/mediumZoomPlugin.ts
+++ plugins/features/plugin-medium-zoom/src/node/mediumZoomPlugin.ts
@@ -1,39 +1,12 @@
-import type { ZoomOptions } from 'medium-zoom'
 import type { Plugin } from 'vuepress/core'
 import { getDirname, path } from 'vuepress/utils'
+import type { MediumZoomPluginOptions } from './options'
 
 const __dirname = import.meta.dirname || getDirname(import.meta.url)
-
-/**
- * Options for @vuepress/plugin-medium-zoom
- */
-export interface MediumZoomPluginOptions {
-  /**
-   * Selector of zoomable images
-   *
-   * @default '[vp-content] > img, [vp-content] :not(a) > img'
-   */
-  selector?: string
-
-  /**
-   * Delay in milliseconds
-   *
-   * @default 500
-   */
-  delay?: number
-
-  /**
-   * Options for medium-zoom
-   *
-   * @see https://github.com/francoischalifour/medium-zoom#options
-   */
-  zoomOptions?: ZoomOptions
-}
 
 export const mediumZoomPlugin = ({
   selector = '[vp-content] > img, [vp-content] :not(a) > img',
   zoomOptions = {},
-  delay = 500,
 }: MediumZoomPluginOptions = {}): Plugin => ({
   name: '@vuepress/plugin-medium-zoom',
 
@@ -42,6 +15,5 @@ export const mediumZoomPlugin = ({
   define: {
     __MZ_SELECTOR__: selector,
     __MZ_ZOOM_OPTIONS__: zoomOptions,
-    __MZ_DELAY__: delay,
   },
 })
