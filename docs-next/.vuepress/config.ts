6584df314c4c0574dc3623bf5163c3fd0aba0d32
Mister-Hope <mister-hope@outlook.com>
2025-02-24 15:00:08 +0800

    docs: update docs config

--- docs-next/.vuepress/config.ts
+++ docs-next/.vuepress/config.ts
@@ -2,28 +2,12 @@ import process from 'node:process'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 import { getModulePath } from '@vuepress/helper'
-import { cachePlugin } from '@vuepress/plugin-cache'
-import { catalogPlugin } from '@vuepress/plugin-catalog'
-import { commentPlugin } from '@vuepress/plugin-comment'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-import { feedPlugin } from '@vuepress/plugin-feed'
-import { iconPlugin } from '@vuepress/plugin-icon'
-import { markdownExtPlugin } from '@vuepress/plugin-markdown-ext'
-import { markdownImagePlugin } from '@vuepress/plugin-markdown-image'
-import { markdownIncludePlugin } from '@vuepress/plugin-markdown-include'
-import { markdownMathPlugin } from '@vuepress/plugin-markdown-math'
-import { markdownStylizePlugin } from '@vuepress/plugin-markdown-stylize'
-import { redirectPlugin } from '@vuepress/plugin-redirect'
-import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
-import { revealJsPlugin } from '@vuepress/plugin-revealjs'
 import type { DefaultThemePageData } from '@vuepress/theme-default/lib/shared/page.js'
 import type { Page } from 'vuepress'
 import { defineUserConfig } from 'vuepress'
-import { getDirname, path } from 'vuepress/utils'
-import { head } from './configs/index.js'
+import { path } from 'vuepress/utils'
+import { head, plugins } from './configs/index.js'
 import theme from './theme.js'
-
-const __dirname = import.meta.dirname || getDirname(import.meta.url)
 
 export default defineUserConfig({
   // set site base to default value
@@ -75,85 +59,7 @@ export default defineUserConfig({
   theme,
 
   // use plugins
-  plugins: [
-    catalogPlugin(),
-    commentPlugin({ provider: 'Giscus' }),
-    docsearchPlugin(),
-    feedPlugin({
-      hostname: 'https://ecosystem.vuejs.press',
-      atom: true,
-      json: true,
-      rss: true,
-    }),
-    iconPlugin({
-      prefix: 'lucide:',
-    }),
-    markdownExtPlugin({
-      gfm: true,
-      component: true,
-      vPre: true,
-    }),
-    markdownImagePlugin({
-      figure: true,
-      mark: true,
-      size: true,
-    }),
-    markdownIncludePlugin({
-      deep: true,
-    }),
-    markdownMathPlugin({
-      type: 'katex',
-    }),
-    markdownStylizePlugin({
-      align: true,
-      attrs: true,
-      mark: true,
-      spoiler: true,
-      sub: true,
-      sup: true,
-      custom: [
-        {
-          matcher: 'Recommended',
-          replacer: ({ tag }) => {
-            if (tag === 'em')
-              return {
-                tag: 'Badge',
-                attrs: { type: 'tip' },
-                content: 'Recommended',
-              }
-
-            return null
-          },
-        },
-      ],
-    }),
-    redirectPlugin({
-      switchLocale: 'modal',
-    }),
-    revealJsPlugin({
-      plugins: ['highlight', 'math', 'search', 'notes', 'zoom'],
-      themes: [
-        'auto',
-        'beige',
-        'black',
-        'blood',
-        'league',
-        'moon',
-        'night',
-        'serif',
-        'simple',
-        'sky',
-        'solarized',
-        'white',
-      ],
-    }),
-    registerComponentsPlugin({
-      components: {
-        NpmBadge: path.resolve(__dirname, './components/NpmBadge.vue'),
-      },
-    }),
-    cachePlugin(),
-  ],
+  plugins,
 
   pagePatterns: ['**/*.md', '!**/*.snippet.md', '!.vuepress', '!node_modules'],
 
