ae03218045227b41cd5b55eefc9f851eb8c4a3a5
pengzhanbo <volodymyr@foxmail.com>
2025-03-05 17:32:43 +0800

    chore: tweak

--- plugins/development/plugin-git/src/client/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientConfig } from 'vuepress/client'
-import { Changelog, Contributors } from './components/index.js'
-
-export default defineClientConfig({
-  enhance({ app }) {
-    app.component('GitChangelog', Changelog)
-    app.component('GitContributors', Contributors)
-  },
-})
