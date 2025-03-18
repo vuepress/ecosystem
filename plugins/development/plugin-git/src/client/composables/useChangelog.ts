29eb4ad7493c65bebfb5ba66217d07144dff8160
Mister-Hope <mister-hope@outlook.com>
2025-03-18 11:37:53 +0800

    chore: tweaks

--- plugins/development/plugin-git/src/client/composables/useChangelog.ts
+++ plugins/development/plugin-git/src/client/composables/useChangelog.ts
@@ -9,56 +9,63 @@ import type {
 import { gitOptions } from '../options.js'
 import { resolveRepoLink } from '../utils/index.js'
 
+declare const __GIT_CHANGELOG__: boolean
+
 export interface GitChangelogItem extends GitChangelogInfo {
   date: string
 }
 
 const RE_ISSUE = /#(\d+)/g
 
-export const useChangelog = (): ComputedRef<GitChangelogItem[]> => {
-  const frontmatter = usePageFrontmatter<GitPluginFrontmatter>()
-  const lang = usePageLang()
-  const page = usePageData<GitPluginPageData>()
+export const useChangelog =
+  typeof __GIT_CHANGELOG__ === 'boolean' && __GIT_CHANGELOG__
+    ? (): ComputedRef<GitChangelogItem[]> => {
+        const frontmatter = usePageFrontmatter<GitPluginFrontmatter>()
+        const lang = usePageLang()
+        const page = usePageData<GitPluginPageData>()
 
-  const { pattern = {}, provider } = gitOptions
-  const repo = resolveRepoLink(gitOptions.repo, provider)
+        const { pattern = {}, provider } = gitOptions
+        const repo = resolveRepoLink(gitOptions.repo, provider)
 
-  return computed(() => {
-    if (frontmatter.value.changelog === false) return []
+        return computed(() => {
+          if (frontmatter.value.changelog === false) return []
 
-    const formatter = new Intl.DateTimeFormat(lang.value, {
-      dateStyle: 'short',
-    })
+          const formatter = new Intl.DateTimeFormat(lang.value, {
+            dateStyle: 'short',
+          })
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    return (page.value.git?.changelog ?? []).map((item) => {
-      const res: GitChangelogItem = {
-        date: formatter.format(item.time),
-        ...item,
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          return (page.value.git?.changelog ?? []).map((item) => {
+            const res: GitChangelogItem = {
+              date: formatter.format(item.time),
+              ...item,
+            }
+
+            if (pattern.issue && repo) {
+              res.message = res.message.replace(
+                RE_ISSUE,
+                (matched, issue: string) => {
+                  const url = pattern
+                    .issue!.replace(':issue', issue)
+                    .replace(':repo', repo)
+                  return `<a href="${url}" target="_blank" rel="noopener noreferrer">${matched}</a>`
+                },
+              )
+            }
+
+            if (pattern.commit && repo) {
+              res.commitUrl = pattern.commit
+                .replace(':hash', res.hash)
+                .replace(':repo', repo)
+            }
+
+            if (pattern.tag && repo && res.tag)
+              res.tagUrl = pattern.tag
+                .replace(':tag', res.tag)
+                .replace(':repo', repo)
+
+            return res
+          })
+        })
       }
-
-      if (pattern.issue && repo) {
-        res.message = res.message.replace(
-          RE_ISSUE,
-          (matched, issue: string) => {
-            const url = pattern
-              .issue!.replace(':issue', issue)
-              .replace(':repo', repo)
-            return `<a href="${url}" target="_blank" rel="noopener noreferrer">${matched}</a>`
-          },
-        )
-      }
-
-      if (pattern.commit && repo) {
-        res.commitUrl = pattern.commit
-          .replace(':hash', res.hash)
-          .replace(':repo', repo)
-      }
-
-      if (pattern.tag && repo && res.tag)
-        res.tagUrl = pattern.tag.replace(':tag', res.tag).replace(':repo', repo)
-
-      return res
-    })
-  })
-}
+    : (): ComputedRef<GitChangelogItem[]> => computed(() => [])
--- plugins/development/plugin-git/src/client/composables/useContributors.ts
+++ plugins/development/plugin-git/src/client/composables/useContributors.ts
@@ -7,14 +7,19 @@ import type {
   GitPluginPageData,
 } from '../../shared/index.js'
 
-export const useContributors = (): ComputedRef<GitContributorInfo[]> => {
-  const frontmatter = usePageFrontmatter<GitPluginFrontmatter>()
-  const page = usePageData<GitPluginPageData>()
+declare const __GIT_CONTRIBUTORS__: boolean
 
-  return computed(() => {
-    if (frontmatter.value.contributors === false) return []
+export const useContributors =
+  typeof __GIT_CONTRIBUTORS__ === 'boolean' && __GIT_CONTRIBUTORS__
+    ? (): ComputedRef<GitContributorInfo[]> => {
+        const frontmatter = usePageFrontmatter<GitPluginFrontmatter>()
+        const page = usePageData<GitPluginPageData>()
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    return page.value.git?.contributors ?? []
-  })
-}
+        return computed(() => {
+          if (frontmatter.value.contributors === false) return []
+
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          return page.value.git?.contributors ?? []
+        })
+      }
+    : (): ComputedRef<GitContributorInfo[]> => computed(() => [])
