e5f213c6f9ee70804bf17efb47096a87b53c814a
Mister-Hope <mister-hope@outlook.com>
2025-05-07 18:28:32 +0800

    fix: add missing slot

--- tools/helper/src/client/typings/slot.ts
+++ tools/helper/src/client/typings/slot.ts
@@ -3,3 +3,4 @@ import type { VNode } from 'vue'
 export type SlotContent = VNode | VNode[] | string | null | undefined
 export type NonNullableSlotContent = NonNullable<SlotContent>
 export type SingleSlotContent = VNode | string | null | undefined
+export type Slot = () => SlotContent
