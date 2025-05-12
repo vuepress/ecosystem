bca0562e4914366898f348af9af8fbb606bfe0e2
Mister-Hope <mister-hope@outlook.com>
2025-05-13 03:42:36 +0800

    fix(theme-default): fix navbar mismatch

--- themes/theme-default/src/client/composables/useNavbarSelectLanguage.ts
+++ themes/theme-default/src/client/composables/useNavbarSelectLanguage.ts
@@ -1,7 +1,7 @@
 import { useData } from '@theme/useData'
 import { useRoutePaths } from '@vuepress/helper/client'
 import type { ComputedRef } from 'vue'
-import { computed } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vuepress/client'
 import type { NavbarItem } from '../typings.js'
 
@@ -12,6 +12,12 @@ export const useNavbarSelectLanguage = (): ComputedRef<NavbarItem[]> => {
   const route = useRoute()
   const routePaths = useRoutePaths()
   const { routeLocale, site, siteLocale, theme, themeLocale } = useData()
+
+  const isMounted = ref(false)
+
+  onMounted(() => {
+    isMounted.value = true
+  })
 
   return computed<NavbarItem[]>(() => {
     const localePaths = Object.keys(site.value.locales)
@@ -43,7 +49,7 @@ export const useNavbarSelectLanguage = (): ComputedRef<NavbarItem[]> => {
           return {
             text,
             activeMatch: '.',
-            link: route.fullPath,
+            link: isMounted.value ? currentFullPath : currentPath,
           }
         }
 
@@ -59,7 +65,9 @@ export const useNavbarSelectLanguage = (): ComputedRef<NavbarItem[]> => {
           text,
           // try to keep current hash and params across languages
           link: routePaths.value.some((item) => item === targetLocalePage)
-            ? currentFullPath.replace(currentPath, targetLocalePage)
+            ? isMounted.value
+              ? currentFullPath.replace(currentPath, targetLocalePage)
+              : targetLocalePage
             : (targetThemeLocale.home ?? targetLocalePath),
         }
       }),
