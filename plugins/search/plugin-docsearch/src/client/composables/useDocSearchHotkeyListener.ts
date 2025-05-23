b908563f820359d8086bcd8bee6ce0b82e126144
Xavier Ruiz <github@xav.ie>
2025-05-22 21:03:34 -0400

    fix(plugin-docsearch): open on ctrl+k when page first loads
    The `{ once: true }` prevented the listener from properly attaching
    and listening for a whole ctrl+k event. It would exit early, causing
    the page to not function properly on first load.

--- plugins/search/plugin-docsearch/src/client/composables/useDocSearchHotkeyListener.ts
+++ plugins/search/plugin-docsearch/src/client/composables/useDocSearchHotkeyListener.ts
@@ -4,19 +4,15 @@ import { useEventListener } from '@vueuse/core'
  * Add hotkey listener, remove it after triggered
  */
 export const useDocSearchHotkeyListener = (callback: () => void): void => {
-  useEventListener(
-    'keydown',
-    (event) => {
-      const isHotKeyBind = event.key === 'k' && (event.ctrlKey || event.metaKey)
-      const isSlashKey = event.key === '/'
+  useEventListener('keydown', (event) => {
+    const isHotKeyBind = event.key === 'k' && (event.ctrlKey || event.metaKey)
+    const isSlashKey = event.key === '/'
 
-      if (!isSlashKey && !isHotKeyBind) {
-        return
-      }
+    if (!isSlashKey && !isHotKeyBind) {
+      return
+    }
 
-      event.preventDefault()
-      callback()
-    },
-    { once: true },
-  )
+    event.preventDefault()
+    callback()
+  })
 }
