536079a565d8ac37965156eb7d57e5f4c95c584c
pengzhanbo <volodymyr@foxmail.com>
2025-05-31 20:36:37 +0800

    chore: e2e test

--- e2e/tests/plugin-theme-data/theme-data.spec.ts
+++ e2e/tests/plugin-theme-data/theme-data.spec.ts
@@ -158,27 +158,9 @@ test.describe('plugin-theme-data', () => {
           },
         },
       },
-      colorMode: 'auto',
-      colorModeSwitch: true,
-      repo: null,
-      selectLanguageText: 'Languages',
-      selectLanguageAriaLabel: 'Select language',
-      sidebarDepth: 2,
       editLink: true,
-      editLinkText: 'Edit this page',
       lastUpdated: true,
       contributors: true,
-      contributorsText: 'Contributors',
-      notFound: [
-        "There's nothing here.",
-        'How did we get here?',
-        "That's a Four-Oh-Four.",
-        "Looks like we've got some broken links.",
-      ],
-      backToHome: 'Take me home',
-      openInNewWindow: 'open in new window',
-      toggleColorMode: 'toggle color mode',
-      toggleSidebar: 'toggle sidebar',
     })
 
     await page.locator('.actions .vp-button').first().click()
