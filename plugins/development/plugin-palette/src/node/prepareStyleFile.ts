c0e14ef2e5ba22b6dd74f2a54eb9ba32971c6692
Mister-Hope <mister-hope@outlook.com>
2025-06-04 03:25:14 +0800

    refactor(plugin-palette): improve jsdoc and docs (#494)

--- plugins/development/plugin-palette/src/node/prepareStyleFile.ts
+++ plugins/development/plugin-palette/src/node/prepareStyleFile.ts
@@ -2,6 +2,24 @@ import type { App } from 'vuepress/core'
 import { fs } from 'vuepress/utils'
 import type { PalettePluginOptions } from './palettePlugin.js'
 
+/**
+ * Prepare style file
+ *
+ * 准备样式文件
+ *
+ * @param app - VuePress app instance
+ * @param options - prepare options
+ * @returns file path of generated style file
+ *
+ * @example
+ * ```ts
+ * const stylePath = await prepareStyleFile(app, {
+ *   userStyleFile: '.vuepress/styles/index.scss',
+ *   tempStyleFile: 'styles/index.scss',
+ *   importCode: (filePath) => `@forward 'file:///${filePath}';\n`
+ * })
+ * ```
+ */
 export const prepareStyleFile = async (
   app: App,
   {
