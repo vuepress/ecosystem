4284df1d3220324f314401287e88fd780ba77fc5
Mister-Hope <mister-hope@outlook.com>
2025-06-04 14:53:50 +0800

    refactor(plugin-nprogress): add jsdoc (#507)

--- docs/zh/plugins/features/nprogress.md
+++ docs/zh/plugins/features/nprogress.md
@@ -6,7 +6,7 @@ icon: pajamas:progress
 
 <NpmBadge package="@vuepress/plugin-nprogress" />
 
-将 [nprogress](https://github.com/rstacruz/nprogress) 集成到 VuePress 中，在切换到另一个页面时会展示进度条。
+将 [nprogress](https://github.com/rstacruz/nprogress) 集成到 VuePress 中，在切换到另一个页面时提供进度条。
 
 该插件已经集成到默认主题中。
 
--- plugins/features/plugin-nprogress/src/node/nprogressPlugin.ts
+++ plugins/features/plugin-nprogress/src/node/nprogressPlugin.ts
@@ -3,6 +3,20 @@ import { getDirname, path } from 'vuepress/utils'
 
 const __dirname = import.meta.dirname || getDirname(import.meta.url)
 
+/**
+ * NProgress plugin for VuePress
+ *
+ * VuePress 的 NProgress 插件
+ *
+ * @example
+ * ```ts
+ * import { nprogressPlugin } from '@vuepress/plugin-nprogress'
+ *
+ * export default {
+ *   plugins: [nprogressPlugin()],
+ * }
+ * ```
+ */
 export const nprogressPlugin = (): Plugin => ({
   name: '@vuepress/plugin-nprogress',
 
