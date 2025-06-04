5eceb2580db123eca8ae182a6af6f86e0cde70d2
Mister-Hope <mister-hope@outlook.com>
2025-06-04 17:52:18 +0800

    docs(plugin-markdown-math): improve docs (#517)

--- plugins/markdown/plugin-markdown-math/src/node/options.ts
+++ plugins/markdown/plugin-markdown-math/src/node/options.ts
@@ -1,12 +1,19 @@
 import type { MarkdownItKatexOptions } from '@mdit/plugin-katex-slim'
 import type { MarkdownItMathjaxOptions } from '@mdit/plugin-mathjax-slim'
 
+/**
+ * Options for KaTeX plugin
+ *
+ * KaTeX 插件选项
+ */
 export interface MarkdownKatexPluginOptions
   extends Omit<MarkdownItKatexOptions, 'transformer'> {
   type?: 'katex'
 
   /**
    * Whether enable copy plugin
+   *
+   * 是否启用复制插件
    *
    * @default false
    */
@@ -15,16 +22,28 @@ export interface MarkdownKatexPluginOptions
   /**
    * Whether enable mhchem plugin
    *
+   * 是否启用 mhchem 插件
+   *
    * @default false
    */
   mhchem?: boolean
 }
 
+/**
+ * Options for MathJax plugin
+ *
+ * MathJax 插件选项
+ */
 export interface MarkdownMathjaxPluginOptions
   extends Omit<MarkdownItMathjaxOptions, 'transformer'> {
   type?: 'mathjax'
 }
 
+/**
+ * Options for Math plugin
+ *
+ * 数学插件选项
+ */
 export type MarkdownMathPluginOptions =
   | MarkdownKatexPluginOptions
   | MarkdownMathjaxPluginOptions
