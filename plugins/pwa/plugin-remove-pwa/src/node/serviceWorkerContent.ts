d56a60414d0de14509109ee11742c6a62ab2cae8
Mister-Hope <mister-hope@outlook.com>
2025-06-05 17:11:16 +0800

    feat(plugin-remove-pwa): improve cache handling

---
+++ plugins/pwa/plugin-remove-pwa/src/node/serviceWorkerContent.ts
@@ -0,0 +1,54 @@
+import type { App } from 'vuepress'
+import { removeLeadingSlash } from 'vuepress/shared'
+import { fs } from 'vuepress/utils'
+
+/**
+ * Generate an empty service worker that cleans caches and unregisters itself
+ *
+ * 生成一个清理缓存并注销自身的空 service worker
+ *
+ * @param app - VuePress App
+ * @param swLocation - Service worker location / Service worker 位置
+ * @param cachePatterns - Cache patterns / 缓存模式
+ * @returns Promise that resolves when the service worker is generated / 生成 service worker 后解析的 Promise
+ */
+export const generateEmptyServiceWorker = async (
+  app: App,
+  swLocation: string,
+  cachePatterns: string[],
+): Promise<void> => {
+  const serviceWorkerContent = `
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', async (event) => {
+  try {
+    await self.clients.claim();
+    await self.registration.unregister();
+
+    const cachePatterns = ${JSON.stringify(cachePatterns)}.map((pattern) => new RegExp(pattern));
+
+    await Promise.all(
+      await self.caches.keys()
+        .filter((name) => !cachePatterns.length || cachePatterns.some((pattern) => pattern.test(name)))
+        .map((name) => self.caches.delete(name)),
+    );
+
+    const clients = await self.clients.matchAll({ type: 'window' });
+
+    clients.forEach((client) => {
+      client.navigate(client.url);
+    });
+  } catch (error) {
+    console.error('Error during deactivation and cleanup:', error);
+  }
+});
+`
+
+  await fs.writeFile(
+    app.dir.dest(removeLeadingSlash(swLocation)),
+    serviceWorkerContent,
+    'utf-8',
+  )
+}
