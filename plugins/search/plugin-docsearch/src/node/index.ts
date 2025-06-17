d340bd4f195e1f4bb7e68d96379d9b3e7b4d399a
Mr.Hope <mister-hope@outlook.com>
2025-06-18 02:25:19 +0800

    chore(plugin-docsearch): tweaks

--- plugins/search/plugin-docsearch/src/node/index.ts
+++ plugins/search/plugin-docsearch/src/node/index.ts
@@ -1,2 +1,3 @@
 export * from './docsearchPlugin.js'
+export type * from './options.js'
 export type * from '../shared/index.js'
---
+++ plugins/search/plugin-docsearch/src/node/options.ts
@@ -0,0 +1,26 @@
+import type { DocSearchOptions } from '../shared/index.js'
+
+/**
+ * Options for @vuepress/plugin-docsearch
+ *
+ * @vuepress/plugin-docsearch 的配置项
+ */
+export interface DocSearchPluginOptions extends DocSearchOptions {
+  /**
+   * Base path of the search index
+   *
+   * 搜索索引的基础路径
+   *
+   * @default app.siteData.base
+   */
+  indexBase?: string
+
+  /**
+   * Whether to inject docsearch default styles
+   *
+   * 是否注入 docsearch 的默认样式
+   *
+   * @default true
+   */
+  injectStyles?: boolean
+}
