763d5ae53fd78e7011407f2986728ab8bce52394
pengzhanbo <volodymyr@foxmail.com>
2025-06-27 16:55:25 +0800

    chore: tweak

--- plugins/tools/plugin-auto-frontmatter/rollup.config.ts
+++ plugins/tools/plugin-auto-frontmatter/rollup.config.ts
@@ -1,5 +1,5 @@
 import { rollupBundle } from '../../../scripts/rollup.js'
 
 export default rollupBundle('node/index', {
-  external: ['chokidar', 'micromatch', 'p-map', 'js-yaml', 'gray-matter'],
+  external: ['chokidar', 'picomatch', 'p-map', 'js-yaml', 'gray-matter'],
 })
