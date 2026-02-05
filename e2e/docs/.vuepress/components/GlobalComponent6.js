f2c53983e12d6484442d3d5da37409071b99b6f8
pengzhanbo <volodymyr@foxmail.com>
2026-02-05 23:55:24 +0800

    chore: tweak

--- e2e/docs/.vuepress/components/GlobalComponent6.js
+++ e2e/docs/.vuepress/components/GlobalComponent6.js
@@ -1,6 +1,6 @@
 import { h } from 'vue'
 
 const GlobalComponent6 = () =>
-  h('div', { id: 'global-component-5' }, 'Global Component 5')
+  h('div', { id: 'global-component-6' }, 'Global Component 5')
 
 export default GlobalComponent6
