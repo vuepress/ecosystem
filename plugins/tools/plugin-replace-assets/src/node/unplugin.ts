826bcedeee6bc11d16b3eb585b318ee70982f588
pengzhanbo <volodymyr@foxmail.com>
2026-03-11 06:47:05 +0800

    fix(plugin-replace-assets): fix `enforce` should be the default in webpack (#639)

--- plugins/tools/plugin-replace-assets/src/node/unplugin.ts
+++ plugins/tools/plugin-replace-assets/src/node/unplugin.ts
@@ -8,11 +8,14 @@ import { transformAssets } from './transformAssets.js'
 import type { ReplacementRule } from './types.js'
 import { createAssetPattern } from './utils.js'
 
-const replaceAssetsFactory: UnpluginFactory<ReplacementRule[]> = (rules) => {
+const replaceAssetsFactory: UnpluginFactory<ReplacementRule[]> = (
+  rules,
+  meta,
+) => {
   const pattern = createAssetPattern('/[^/]')
   return {
     name: 'vuepress:replace-assets',
-    enforce: 'pre',
+    enforce: meta?.framework === 'vite' ? 'pre' : undefined,
     transform: {
       filter: { id: { exclude: [/\.json(?:$|\?)/, /\.html?$/] } },
       handler(code) {
