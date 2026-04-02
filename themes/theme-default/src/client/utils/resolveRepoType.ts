12ea0b99d60fa070ce73464fe472acb023904f57
Mr.Hope <mister-hope@outlook.com>
2026-04-02 10:32:07 +0800

    fix(theme): stricter repo resolve

--- themes/theme-default/src/client/utils/resolveRepoType.ts
+++ themes/theme-default/src/client/utils/resolveRepoType.ts
@@ -4,8 +4,10 @@ export type RepoType = 'Bitbucket' | 'Gitee' | 'GitHub' | 'GitLab' | null
 
 export const resolveRepoType = (repo: string): RepoType => {
   if (!isLinkHttp(repo) || repo.includes('github.com')) return 'GitHub'
-  if (repo.includes('bitbucket.org')) return 'Bitbucket'
-  if (repo.includes('gitlab.com')) return 'GitLab'
-  if (repo.includes('gitee.com')) return 'Gitee'
+
+  if (/^(https?:)?\/\/bitbucket\.org\//.test(repo)) return 'Bitbucket'
+  if (/^(https?:)?\/\/gitlab\.com\//.test(repo)) return 'GitLab'
+  if (/^(https?:)?\/\/gitee\.com\//.test(repo)) return 'Gitee'
+
   return null
 }
