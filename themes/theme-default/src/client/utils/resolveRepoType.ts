3767165cb99c814bfd66eea9a761b394a4ba90c3
Mr.Hope <mister-hope@outlook.com>
2026-04-02 11:44:42 +0800

    fix(theme-default): improve repo resolve

--- themes/theme-default/src/client/utils/resolveRepoType.ts
+++ themes/theme-default/src/client/utils/resolveRepoType.ts
@@ -3,7 +3,8 @@ import { isLinkHttp } from 'vuepress/shared'
 export type RepoType = 'Bitbucket' | 'Gitee' | 'GitHub' | 'GitLab' | null
 
 export const resolveRepoType = (repo: string): RepoType => {
-  if (!isLinkHttp(repo) || repo.includes('github.com')) return 'GitHub'
+  if (!isLinkHttp(repo) || /^(https?:)?\/\/github\.com\//.test(repo))
+    return 'GitHub'
 
   if (/^(https?:)?\/\/bitbucket\.org\//.test(repo)) return 'Bitbucket'
   if (/^(https?:)?\/\/gitlab\.com\//.test(repo)) return 'GitLab'
